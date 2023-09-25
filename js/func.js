745a2374ac87362ee69f57f2a1173f8a9e38d0ec
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-25 11:28:03 -0600

    Update func.js
    otro

--- js/func.js
+++ js/func.js
@@ -1,8 +1,11 @@
-let $msg = (t,m)=>{ let $m = $("div").html($("div").css({"color":"red","width":"100%"}).html(t));
+
+
+
+
+$("#hoy").click(x=>{ 
+  let $msg = (t,m)=>{ let $m = $("div").html($("div").css({"color":"red","width":"100%"}).html(t));
                         $m.append($("div").css({"color":"black","width":"100%"}).html(m)).attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"}) 
                       return $m
-                  }
-
-
-
-$("#hoy").click(x=>{ $("body").append($msg("Hoy es",new Date()))})
+                    }
+  $("body").append($msg("Hoy es",new Date()))
+})
