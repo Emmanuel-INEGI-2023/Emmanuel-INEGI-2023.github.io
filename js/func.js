5869c69a53b18459b5bed5d16e6c521a0a0fad78
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-25 11:39:47 -0600

    Update func.js
    agregar  <>

--- js/func.js
+++ js/func.js
@@ -3,9 +3,9 @@
 
 
 $("#hoy").click(x=>{ 
-  let $msg = (t,m)=>{ let $ms = $("div").attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"});
-                        $ms.append($("div").css({"color":"black","width":"100%"}).html(m)).append($("div").css({"color":"red","width":"100%"}).html(t))  
-                      return $ms
+  let $msg = (t,m)=>{ let $ms = $("<div>").attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"});
+                          $ms.append($("<div>").css({"color":"black","width":"100%"}).html(m)).append($("<div>").css({"color":"red","width":"100%"}).html(t));
+                         return $ms;
                     }
   $("body").append($msg("Hoy es",new Date()))
 })
