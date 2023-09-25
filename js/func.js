aa8bc0d78a0eeef16ae44c63a53023a08777a8da
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-25 11:19:43 -0600

    Update func.js
    Corregir bien el mensaje

--- js/func.js
+++ js/func.js
@@ -1,4 +1,7 @@
-let $msg = (t,m)=>{return $("div").html($("div").css({"color":"red","width":"100%"}).html(t) +  $("div").css({"color":"black","width":"100%"}).html(m)).attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"})  }
+let $msg = (t,m)=>{ let $m = $("div").html($("div").css({"color":"red","width":"100%"}).html(t));
+                        $m.append($("div").css({"color":"black","width":"100%"}).html(m)).attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"}) 
+                      return $m
+                  }
 
 
 
