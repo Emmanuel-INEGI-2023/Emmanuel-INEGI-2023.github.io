a6e6abe5728be9a24baf5a661bab0f6de2cd0e40
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-09-25 11:03:05 -0600

    Update func.js
    corregir

--- js/func.js
+++ js/func.js
@@ -1,3 +1,5 @@
-let $msg = (t,m)=>{return $("div").html($("div").css({"color":"red","width":"100%"}).html(t)).append($("div").css({"color":"black","width":"100%"}).html(m)).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%"})}
-  
+let $msg = (t,m)=>{return $("div").html($("div").css({"color":"red","width":"100%"}).html(t);  $("div").css({"color":"black","width":"100%"}).html(m); ).attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"})  }
+
+
+
 $("#hoy").click(x=>{ $("body").append($msg("Hoy es",new Date()))})
