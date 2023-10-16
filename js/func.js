e337b6deebb36ba03838e5e4594042d856a42ba7
Genealizacion (Pruebas) <144199757+Emmanuel-INEGI-2023@users.noreply.github.com>
2023-10-16 21:16:25 +0000

    imagen y lista

--- js/func.js
+++ js/func.js
@@ -1,14 +1,17 @@
+let $ima 
 $(document).ready(e=>{
 
 alert(e)
 $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=bfa177731ce9400d8320e530440591cb", data=>{
                   let _arr = [data.timezone,data.ip_address,data.longitude,data.latitude]
                   $("#datos").html($("<ul>").attr("id","lst"))
-                  $.each(_arr,d=>{$("#lst").append($("<li>").html(d))})
+                  $.each(_arr,()=>{$("#lst").append($("<li>").html(this))})
+                  console.log(_arr)
         })
 
   $.getJSON("https://screenshot.abstractapi.com/v1/?api_key=2a9f856ee13d44829f49f98bf41377f6&url=https://frp.gsmneo.com", data=> {
-      $("#datos").append($("<img>").attr("src",data))
+        $ima =  new Image(data)
+  $("#datos").append($("<div>").html($ima))
 
   })
 
@@ -17,6 +20,7 @@ $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=bfa177731ce9400d832
 
 
 $("#hoy").click(x=>{ 
+  console.log($ima)
   let $msg = (t,m)=>{ let $ms = $("<div>").attr({"id":"mensaje"}).css({"text-align":"center","font-family":"Century Gothic","position":"absolute","width":"200px","left":"45%","top":"48%","background":"crimson","padding":"18px","border":"2px solid gray","border-radius":"6px"});
                           $ms.append($("<div>").css({"color":"black","width":"100%"}).html(m)).append($("<div>").css({"color":"red","width":"100%"}).html(t));
                          return $ms;
